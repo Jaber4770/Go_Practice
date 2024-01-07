ed5cb565ecfdfc6654a55db7ebf8add3c45e3f6c
unknown <jaber.ahmed4770@gmail.com>
2024-01-07 15:44:52 +0200

    learning go, variable

---
+++ Variable.go
@@ -0,0 +1,31 @@
+package main
+import ("fmt")
+
+func main() {
+	fmt.Println("Hello World! from Go")
+
+	//Variable
+	var studentName string = "Jaber Ahmed"
+	var student1 string = "John"
+
+	fmt.Println(studentName)
+	fmt.Println(student1)
+
+
+	// another way to write variable
+	var nameOfDeveloper = "jack sargey"
+	fmt.Println(nameOfDeveloper)
+
+	// another way of varialbe
+
+	developerNumber := 4
+	fmt.Println(developerNumber);
+	
+
+
+}
+
+// this is single line comment system of Go
+/* this is multi line comment system */
+
+
