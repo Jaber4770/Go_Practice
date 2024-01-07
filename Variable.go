36f7706732376f6f507ee14eaa9cfbc97e80d2bd
unknown <jaber.ahmed4770@gmail.com>
2024-01-07 15:55:28 +0200

    go variable learing, it's interesting

--- Variable.go
+++ Variable.go
@@ -1,6 +1,14 @@
 package main
 import ("fmt")
 
+
+var z int
+var y float32 = 3.3
+var x bool = false
+var zx string = "joe"
+
+
+
 func main() {
 	fmt.Println("Hello World! from Go")
 
@@ -20,12 +28,46 @@ func main() {
 
 	developerNumber := 4
 	fmt.Println(developerNumber);
+
+
+	// another way or boolean
+	var jack bool = true
+	fmt.Println(jack)
+	jackSargey := true
+	fmt.Println(jackSargey)
+
+	// another way of var declration
+
+	var a string
+	var b int
+	var c bool
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+
+
+	var money int
+	money = 5000
+	fmt.Println(money)
+
+	var dollar float32
+	dollar = 44.34
+	fmt.Println(dollar)
+
+
+	//
+
 	
+	z = 1
+	fmt.Println(z)
+	fmt.Println(y)
+	fmt.Println(x)
+	fmt.Println(zx)
 
 
 }
 
 // this is single line comment system of Go
 /* this is multi line comment system */
-
 
